a14153093f2757976829ee68d0db98572dbda127
AnotherNoobDev <gebesleaga@gmail.com>
2022-06-07 20:23:06 +0200

    added BottomNavigation

--- app/src/main/java/com/example/gymbud/MainActivity.kt
+++ app/src/main/java/com/example/gymbud/MainActivity.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +22,9 @@ class MainActivity : AppCompatActivity() {
         navController = navHostFragment.navController
 
         setupActionBarWithNavController(navController)
+
+        val navBar = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        navBar.setupWithNavController(navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
