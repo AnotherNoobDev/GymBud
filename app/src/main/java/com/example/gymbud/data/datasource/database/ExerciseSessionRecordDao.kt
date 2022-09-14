efceb7cfbb62a9a85519014c5039155115b23fdb
AnotherNoobDev <gebesleaga@gmail.com>
2022-09-14 20:27:51 +0200

    personal bests shall use 1rm

--- app/src/main/java/com/example/gymbud/data/datasource/database/ExerciseSessionRecordDao.kt
+++ app/src/main/java/com/example/gymbud/data/datasource/database/ExerciseSessionRecordDao.kt
@@ -16,11 +16,11 @@ interface ExerciseSessionRecordDao {
     @Query("SELECT * from exercise_session WHERE workout_session_id = :workoutSessionId")
     suspend fun getFromSession(workoutSessionId: ItemIdentifier): List<ExerciseSessionRecord>
 
-    // todo resistance is a string.. will sorting work correctly?
+    // todo: replace with raw query and use ExerciseEvaluators.calculateOneRepMax instead of naked expression in ORDER BY
     @Query("SELECT workout_session_id, reps, resistance from exercise_session " +
             "WHERE exercise_template_id IN (:exerciseTemplates) " +
             "AND workout_session_id IN (:workoutSessions) " +
-            "ORDER BY resistance DESC " +
+            "ORDER BY resistance * (1 + reps / 30.0) DESC " +
             "LIMIT 1")
     suspend fun getExercisePersonalBest(exerciseTemplates: List<ItemIdentifier>, workoutSessions: List<ItemIdentifier> ): ExerciseResult?
 
