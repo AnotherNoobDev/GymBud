f87bbd89726f1c1144105a18ed08fedef4336afa
AnotherNoobDev <gebesleaga@gmail.com>
2022-09-25 13:02:57 +0200

    fix some texts

--- app/src/main/java/com/example/gymbud/ui/ProgramBuilderFragment.kt
+++ app/src/main/java/com/example/gymbud/ui/ProgramBuilderFragment.kt
@@ -25,14 +25,14 @@ class ProgramBuilderFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.apply {
             exercisesPanel.title.text = "Exercises"
-            exercisesPanel.description.text = "Descriptions of movements to target specific muscles."
+            exercisesPanel.description.text = "An Exercise describes the movement that shall be performed and the (main) muscle group it targets."
             exercisesPanel.card.setOnClickListener {
                 val action = ProgramBuilderFragmentDirections.actionProgramBuilderFragmentToItemListFragment(ItemType.EXERCISE)
                 findNavController().navigate(action)
             }
 
             exerciseTemplatesPanel.title.text = "Exercise Templates"
-            exerciseTemplatesPanel.description.text = "Customizations on top of Exercises, for example rep. ranges."
+            exerciseTemplatesPanel.description.text = "An Exercise Template is an Exercise with a specified rep range. Exercise Templates are the building blocks for Set Templates."
             exerciseTemplatesPanel.card.setOnClickListener {
                 val action = ProgramBuilderFragmentDirections.actionProgramBuilderFragmentToItemListFragment(ItemType.EXERCISE_TEMPLATE)
                 findNavController().navigate(action)
@@ -46,21 +46,21 @@ class ProgramBuilderFragment : Fragment() {
             }
 
             setTemplatesPanel.title.text = "Set Templates"
-            setTemplatesPanel.description.text = "Group of ExerciseTemplates and RestPeriods."
+            setTemplatesPanel.description.text = "A Set Template is composed of Exercise Templates and Rest Periods. Set Templates are the building blocks for Workout Templates."
             setTemplatesPanel.card.setOnClickListener {
                 val action = ProgramBuilderFragmentDirections.actionProgramBuilderFragmentToItemListFragment(ItemType.SET_TEMPLATE)
                 findNavController().navigate(action)
             }
 
             workoutTemplatesPanel.title.text = "Workout Templates"
-            workoutTemplatesPanel.description.text = "Group of Sets and RestPeriods"
+            workoutTemplatesPanel.description.text = "A Workout Template is composed of Sets and Rest Periods. Workout Templates are the building blocks for Programs."
             workoutTemplatesPanel.card.setOnClickListener {
                 val action = ProgramBuilderFragmentDirections.actionProgramBuilderFragmentToItemListFragment(ItemType.WORKOUT_TEMPLATE)
                 findNavController().navigate(action)
             }
 
             programTemplatesPanel.title.text = "Programs"
-            programTemplatesPanel.description.text = "Group of Workouts."
+            programTemplatesPanel.description.text = "A Program is a repeatable training block that spans a given number of days. Each day in a Program is either a Workout or a Rest Day."
             programTemplatesPanel.card.setOnClickListener {
                 val action = ProgramBuilderFragmentDirections.actionProgramBuilderFragmentToItemListFragment(ItemType.PROGRAM_TEMPLATE)
                 findNavController().navigate(action)
