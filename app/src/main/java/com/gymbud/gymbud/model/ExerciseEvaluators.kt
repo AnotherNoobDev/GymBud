abab3ddb2feaa68477c714858e617f7c552f330f
AnotherNoobDev <gebesleaga@gmail.com>
2023-02-07 19:45:28 +0100

    handle invalid reps input

--- app/src/main/java/com/gymbud/gymbud/model/ExerciseEvaluators.kt
+++ app/src/main/java/com/gymbud/gymbud/model/ExerciseEvaluators.kt
@@ -4,11 +4,9 @@ package com.gymbud.gymbud.model
  * Epley formula: https://en.wikipedia.org/wiki/One-repetition_maximum
  */
 fun calculateOneRepMax(reps: Int, resistance: Double): Double {
-    assert(reps > 0)
-
-    return if (reps == 1) {
-        resistance
-    } else {
-        resistance * (1 + reps / 30.0)
+    return when {
+        reps <= 0 -> 0.0
+        reps == 1 -> resistance
+        else -> resistance * (1 + reps / 30.0)
     }
 }
